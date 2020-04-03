49d769ba62245dfb1685dba1c7605bd4d3f2c285
Prestwick97 <prestwick97@gmail.com>
2020-04-03 13:15:02 -0700

    update appsettings.json

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -6,6 +6,7 @@ namespace AnimalShelter.Models
     public int AnimalId { get; set; }
     public string Species { get; set; }
     public int Age { get; set; }
+    public string Gender { get; set; }
  
     public string Name { get; set; }
     public string Info { get; set; }
--- Models/AnimalShelterContext.cs
+++ Models/AnimalShelterContext.cs
@@ -2,13 +2,19 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AnimalShelter.Models
 {
-    public class AnimalShelterContext : DbContext
+  public class AnimalShelterContext : DbContext
+  {
+    public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
+    : base(options)
     {
-        public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<Animal> Animals { get; set; }
     }
+    public DbSet<Animal> Animals { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+  builder.Entity<Animal>()
+      .HasData(
+        new Animal { AnimalId = 1, Species = "cat", Age = 30, Name = "Nepolian", Gender = "male", Info = "Lovely kitty, very nice and lovely."}
+      );
+    }
+  }
 }
