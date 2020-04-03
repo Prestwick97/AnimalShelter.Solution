d2285b31be0d344fa43fa32009999a297deedcd9
Prestwick97 <prestwick97@gmail.com>
2020-04-03 15:00:34 -0700

    remove versioning for testing

--- Controllers/AnimalsController.cs
+++ Controllers/AnimalsController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using AnimalShelter.Models;
+using System;
 
 namespace AnimalShelter.Controllers
 {
--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -6,9 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AnimalShelter.Controllers
 {
-  [ApiVersion("1.0")]
-  [Route("api/{v:apiVersion}/values")]
-  //api/1.0/values
+  [Route("api/[controller]")]
   [ApiController]
   public class ValuesController : ControllerBase
   {
@@ -16,7 +14,7 @@ namespace AnimalShelter.Controllers
     [HttpGet]
     public ActionResult<IEnumerable<string>> Get()
     {
-      return new string[] { "Value1 from Version1 ", "value2 from Version 1" };
+      return new string[] { "value1", "value2" };
     }
 
     // GET api/values/5
@@ -30,45 +28,6 @@ namespace AnimalShelter.Controllers
     [HttpPost]
     public void Post([FromBody] string value)
     {
-      Console.WriteLine(value);
-    }
-
-    // PUT api/values/5
-    [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
-    {
-    }
-
-    // DELETE api/values/5
-    [HttpDelete("{id}")]
-    public void Delete(int id)
-    {
-    }
-  }
-  [ApiVersion("2.0")]
-  [Route("api/{v:apiVersion}/values")]
-  [ApiController]
-  public class ValuesTwoController : ControllerBase
-  {
-    // GET api/values
-    [HttpGet]
-    public ActionResult<IEnumerable<string>> Get()
-    {
-      return new string[] { "value1 from Version 2", "value2 from Version 2" };
-    }
-
-    // GET api/values/5
-    [HttpGet("{id}")]
-    public ActionResult<string> Get(int id)
-    {
-      return "value";
-    }
-
-    // POST api/values
-    [HttpPost]
-    public void Post([FromBody] string value)
-    {
-      Console.WriteLine(value);
     }
 
     // PUT api/values/5
@@ -84,3 +43,91 @@ namespace AnimalShelter.Controllers
     }
   }
 }
+
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Mvc;
+
+// namespace MessageBoard.Controllers
+// {
+//   [ApiVersion("1.0")]
+//   [Route("api/{v:apiVersion}/values")]
+//   //api/1.0/values
+//   [ApiController]
+//   public class ValuesController : ControllerBase
+//   {
+//     // GET api/values
+//     [HttpGet]
+//     public ActionResult<IEnumerable<string>> Get()
+//     {
+//       return new string[] { "Value1 from Version1 ", "value2 from Version 1" };
+//     }
+
+//     // GET api/values/5
+//     [HttpGet("{id}")]
+//     public ActionResult<string> Get(int id)
+//     {
+//       return "value";
+//     }
+
+//     // POST api/values
+//     [HttpPost]
+//     public void Post([FromBody] string value)
+//     {
+//       Console.WriteLine(value);
+//     }
+
+//     // PUT api/values/5
+//     [HttpPut("{id}")]
+//     public void Put(int id, [FromBody] string value)
+//     {
+//     }
+
+//     // DELETE api/values/5
+//     [HttpDelete("{id}")]
+//     public void Delete(int id)
+//     {
+//     }
+//   }
+//   [ApiVersion("2.0")]
+//   [Route("api/{v:apiVersion}/values")]
+//   [ApiController]
+//   public class ValuesTwoController : ControllerBase
+//   {
+//     // GET api/values
+//     [HttpGet]
+//     public ActionResult<IEnumerable<string>> Get()
+//     {
+//       return new string[] { "value1 from Version 2", "value2 from Version 2" };
+//     }
+
+//     // GET api/values/5
+//     [HttpGet("{id}")]
+//     public ActionResult<string> Get(int id)
+//     {
+//       return "value";
+//     }
+
+//     // POST api/values
+//     [HttpPost]
+//     public void Post([FromBody] string value)
+//     {
+//       Console.WriteLine(value);
+//     }
+
+//     // PUT api/values/5
+//     [HttpPut("{id}")]
+//     public void Put(int id, [FromBody] string value)
+//     {
+//     }
+
+//     // DELETE api/values/5
+//     [HttpDelete("{id}")]
+//     public void Delete(int id)
+//     {
+//     }
+//   }
+// }
+
